2519dc10dd35d6ddd0aa091371bf809a031bfa2f
student-a502 <student-a502@STUD-RM20-A502>
2025-11-10 17:50:56 +0500

    Разработанный класс «Phrase». Бартова

---
+++ Classes/Phrase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Бартова.Classes
+{
+    public class Phrase
+    {
+        public string _Phrase { get; set; }
+        public string Src { get; set; }
+        public Phrase(string Phrase, string Src) {
+            this._Phrase = Phrase;
+            this.Src = Src;
+        }
+    }
+}
