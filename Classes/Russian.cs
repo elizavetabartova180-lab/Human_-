eec7e609c23a8483075df1b840bafc32f363552c
student-a502 <student-a502@STUD-RM20-A502>
2025-11-10 18:23:57 +0500

    Разработанный класс «Russian». Бартова

---
+++ Classes/Russian.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Бартова.Classes
+{
+    public class Russian : Human
+    {
+        private List<Phrase> Phrases { get; set; }
+        private int stepAudio;
+        private int StepAudio
+        {
+            get { return stepAudio; }
+            set
+            {
+                stepAudio = value;
+                if (stepAudio > Phrases.Count - 1)
+                    stepAudio = 0;
+            }
+        }
+        public Russian(string Name, string Img) : base(Name, Img)
+        {
+            this.Phrases = AllPhrases();
+        }
+        public override void Speak(Label Phrase)
+        {
+            Phrase.Content = Phrases[StepAudio]._Phrase;
+            MainWindow.MediaPlayer.Open(new Uri(Phrases[StepAudio].Src));
+            MainWindow.MediaPlayer.Play();
+            StepAudio++;
+        }
+        public static List<Phrase> AllPhrases()
+        {
+            List<Phrase> allPhrases = new List<Phrase>();
+            allPhrases.Add(new Phrase("Привет", @"..."));
+            allPhrases.Add(new Phrase("Как дела?", @"..."));
+            allPhrases.Add(new Phrase("Меня зовут Александр.", @"..."));
+            return allPhrases;
+        }
+    }
+}
