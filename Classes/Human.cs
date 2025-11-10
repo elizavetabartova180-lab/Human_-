8a30e07a9eae32c27e4dee0192834b9b77169ee6
student-a502 <student-a502@STUD-RM20-A502>
2025-11-10 17:46:09 +0500

    Разработанный класс «Human». Бартова

---
+++ Classes/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Бартова.Classes
+{
+     public abstract class Human
+    {
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public Human(string name, string Img)
+        {
+            this.Name = name;
+            this.Img = Img;
+        }
+        public abstract void Speak(Label Phrase);
+    }
+}
