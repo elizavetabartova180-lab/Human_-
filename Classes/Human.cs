13719c9d825c477ff68af60db6811794ed1a2f70
student-a502 <student-a502@STUD-RM20-A502>
2025-11-10 19:57:55 +0500

    шаг 19

--- Classes/Human.cs
+++ Classes/Human.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Human_Бартова.Classes
 {
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Human_Бартова.Classes;
 
 namespace Human_Бартова
 {
@@ -20,9 +21,12 @@ namespace Human_Бартова
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MediaPlayer MediaPlayer = new MediaPlayer();
+        private Classes.Russian Russian = new Classes.Russian("Александр", @"C:\Users\student-A502.PERMAVIAT\Desktop\Human_Бартова\Images\ic_russian.png");
         public MainWindow()
         {
             InitializeComponent();
+            parent.Children.Add(new Elements.Item(Russian));
         }
     }
 }
